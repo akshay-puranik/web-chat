80722225b473e31b4f61a5b1f3fbd2bc28e19829
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-11-20 01:09:24 +0530

    socket connect and disconnect methods

--- src/controller/socket.controller.js
+++ src/controller/socket.controller.js
@@ -2,28 +2,40 @@ const users = new Map(); //key should be unique and constant ex. userId
 
 class Connection {
   constructor(io, socket) {
-    this.#io = io;
-    this.#socket = socket;
+    this.io = io;
+    this.socket = socket;
 
+    socket.on("connect", () => this.userConnected(socket));
     socket.on("sendMessage", (message) => {});
+    socket.on("disconnect", () => userDisconnect(socket));
+  }
+
+  userConnected(socket) {
+    users.set(socket.id, new User("User", socket.id));
+    console.log("New User", socket.id);
+  }
+
+  userDisconnect(socket) {
+    let user = users.get(socket.id);
+    user.status = Date.now();
+    users.set(socket.id, user);
+    console.log("New User", socket.id);
   }
 
   sendMessage(message) {
     this.io.sockets.emit("message", message);
   }
 }
-
 class User {
   constructor(name, socketId) {
     this.name = name + "" + socketId;
     this.socketId = socketId;
+    this.status = "Online";
   }
 }
 
 const chat = (io) => {
   io.on("connection", (socket) => {
-    users.set(socket.id, new User("User", socket.id));
-    console.log("New User", socket.id);
     new Connection(io, socket);
   });
 };
